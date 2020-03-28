82aacf99d21dcdb7a844fc93cb0dd902fca08b02
James Saucier <james.saucier@lgm.ca>
2020-03-28 15:54:35 -0700

    initial commit

---
+++ FSwag/AppBuilderExtensions.fs
@@ -0,0 +1,20 @@
+﻿module FSwag.AppBuilderExtensions
+
+open Middleware
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.FileProviders
+open System.Reflection
+
+type IApplicationBuilder with
+    member this.UseSwaggerUI () =
+        let fileProvider = EmbeddedFileProvider(typeof<SwaggerMiddleware>.GetTypeInfo().Assembly, "FSwag.Res.Swagger")
+        let fileServerOptions =
+            let options = FileServerOptions()
+            options.FileProvider <- fileProvider
+            options
+
+        this.UseFileServer(fileServerOptions)
+            .UseMiddleware<SwaggerMiddleware>()
+            
+    member this.UseReDoc =
+        this.UseMiddleware<ReDocMiddleware>()
---
+++ FSwag/Middleware.fs
@@ -0,0 +1,43 @@
+﻿module FSwag.Middleware
+
+open System
+open System.IO;
+open FSharp.Control.Tasks.V2
+open Microsoft.Extensions.Primitives
+open Microsoft.AspNetCore.Http
+
+let private setResponseHeaders (context: HttpContext) =
+    context.Response.Headers.["Content-Type"] <- StringValues("text/html; charset=utf-8")
+    context.Response.StatusCode <- 200
+
+let private writeResponse (context: HttpContext) (documentStream: Stream) =
+    task {
+        do setResponseHeaders context
+        use reader = new StreamReader(documentStream)
+        let! document = reader.ReadToEndAsync()
+        do! context.Response.WriteAsync(document)
+    }
+
+let private isMatch (urlPath: PathString) document =
+    urlPath.HasValue && 
+        String.Equals(urlPath.Value.TrimEnd('/'), document, StringComparison.OrdinalIgnoreCase)
+
+type ReDocMiddleware (next: RequestDelegate) =
+    member this.Invoke(ctx: HttpContext) =
+        task {
+            if isMatch ctx.Request.Path "/redoc" then
+                let redocStream = this.GetType().Assembly.GetManifestResourceStream("FSwag.Res.Redoc.index.html");
+                do! writeResponse ctx redocStream
+            else
+                next.Invoke ctx |> ignore
+        }
+
+type SwaggerMiddleware (next: RequestDelegate) =
+    member this.Invoke(ctx: HttpContext) =
+        task {
+            if isMatch ctx.Request.Path "/swagger" then
+                let swaggerStream = this.GetType().Assembly.GetManifestResourceStream("FSwag.Res.Swagger.index.html");
+                do! writeResponse ctx swaggerStream
+            else
+                next.Invoke ctx |> ignore
+        }
